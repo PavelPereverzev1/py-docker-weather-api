a3ceb3aa8a67e970d28c001322045a9cbe03c004
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-06-07 15:09:03 +0300

    feat: solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,37 @@
+import os
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+
+    API_KEY = os.getenv("API_KEY")
+    if not API_KEY:
+        print("Error: API_KEY is not set!")
+        return
+
+    CITY = "Paris"
+    URL = (
+        f"http://api.weatherapi.com/v1/current.json"
+        f"?key={API_KEY}&q={CITY}&aqi=no"
+    )
+
+    try:
+        response = requests.get(URL)
+        response.raise_for_status()
+        data = response.json()
+
+        location = data["location"]
+        current = data["current"]
+        print(f"Performing request to Weather API for city {CITY}...")
+        print(
+            f"{location['name']}/{location['country']}"
+            f" {location['localtime']} "
+            f"Weather: {current['temp_c']} Celsius,"
+            f" {current['condition']['text']}"
+        )
+
+    except Exception as error:
+        print(f"An error occurred: {error}")
 
 
 if __name__ == "__main__":
